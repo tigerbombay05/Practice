7f3d789eeaae9a57443262ef1dc3486e37dd910b
Tiger Bombay <tigerbombay05@gmail.com>
2026-04-27 21:49:17 +0530

    Basic Calculator

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,15 @@
-print("wazzap")
+no1 = float(input("Enter First Number: "))
+no2 = float(input("Enter Second Number: "))
+operator = input("Enter Operator: ")
+result = 0
+
+if (operator == "+"):
+    result = no1 + no2
+elif (operator == "-"):
+    result = no1 - no2
+elif (operator == "*"):
+    result = no1 * no2
+elif (operator == "/"):
+    result = no1 / no2
+
+print(f"{no1} {operator} {no2} = {result}")
