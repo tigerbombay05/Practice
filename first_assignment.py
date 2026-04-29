9957f296f767621813d197106554ed06862cb6d7
Tiger Bombay <tigerbombay05@gmail.com>
2026-04-30 01:11:43 +0530

    first_assignment

--- first_assignment.py
+++ first_assignment.py
@@ -41,12 +41,12 @@ __|          ;     |MM'MM'''''---..._______...--''MM'MM]                   |
 """  
 print(splash_art)
 
-print("It is a peaceful day in Alostria when suddenly " \
+print("It is a peaceful day in Alostria when suddenly" \
 " slyzamanders emerge from the waters and start attacking the populace.\n")
 
 print()
 
-print("You, the heroic knight Rava, hear of this dreadful news "
+print("You, the heroic knight Rava, hear of this dreadful news"
 " and rush to the royal stables to mount a horse to battle.")
 
 print()
@@ -58,15 +58,15 @@ decision1 = input("Which horse will you mount?\n"
 
 print()
 
-if(decision1 == "Slowie"):
-    print("Rava vastly overestimates his horsemanship " \
+if(decision1 == "1"):
+    print("Rava vastly overestimates his horsemanship" \
     " by taking the worst horse in all of the kingdom to batlle. " \
     "He doesn't reach the battle in time and is thus sacked from knighthood "
-    " and dies alone in a shack.")
+    "and dies alone in a shack.")
 
     print()
 
-elif(decision1 == "Zephyr"):
+elif(decision1 == "2"):
     print("Rava steadies his resolve and mounts a legendarily fast horse. " \
     "He does not have time to tarry! " \
     "He reaches the slyzamanders faster than thought, " \
@@ -77,10 +77,14 @@ elif(decision1 == "Zephyr"):
     print("Rava dismounts the horse and counts 8 big, nasty, slimy " \
     " slyzamanders brandishing jagged sea rock and rotten corals.")
 
+    print()
+
     decision2 = input("How do you respond to the amphibious threat?\n" \
     "1. Appeal to reason\n" \
     "2. Bravely fend them off\n" \
     "Answer: ")
+
+    print()
 
     if(decision2 == "1"):
         print("Slyzamanders aren't exactly known for their diplomacy. " \
@@ -88,23 +92,32 @@ elif(decision1 == "Zephyr"):
         "it isn't long until he is skinned alive with their " \
         "crude instruments as they then pour seawater " \
         "over his bare flesh.")
+
+        print()
+
     elif(decision2 == "2"):
         print("Rava isn't afraid of any slimy sea beast! " \
         "It's nothing he hasn't already seen in his swashbuckling days. " \
         "He wields his trusty sword and fights them off skillfully. " \
-        "He stays the half of them before the rest hiss and retreat " \
+        "He slays the half of them before the rest hiss and retreat " \
         "back to the waters. Victory!")
 
-        print("Although the slyzards are gone, he notices how the immediate " \
-        "area has been ravaged by salty beasts. The merchants return to find " \
+        print()
+
+        print("Although the slyzamanders are gone, he notices how the immediate " \
+        "area has been completely demolished in the ensuing scuffle. The merchants return to find " \
         "their stalls, their entire livelihoods, gone. Rava knows that he has to help, " \
         "so he thinks for a moment...")
+
+        print()
 
         decision3 = input("How does Rava solve this quandary?\n" \
         "1. Tell the merchants to just pull themselves up by their bootstraps and grind & hustle.\n" \
         "2. Pledge your knightly salary to those most in need until they rebuild their life.\n" \
         "3. Suggest that they go into a different industry.\n" \
         "Answer: ")
+
+        print()
 
         if(decision3 == "1"):
             print("ULTRALIBERAL KNIGHT: The citizenry is fascinated " \
@@ -113,6 +126,9 @@ elif(decision1 == "Zephyr"):
             "along with more who got influenced, evolve into money-hungry yuppies " \
             "with more money than sense. Rava gave them their life back, " \
             "but at what true cost?")
+
+            print()
+
         elif(decision3 == "2"):
             print("SELFLESS KNIGHT: Rava doesn't need much to be happy and " \
             "thusly offers his salary to those most affected by the attack. " \
@@ -121,6 +137,9 @@ elif(decision1 == "Zephyr"):
             "lives back in order, but he also gets some return on his investment! " \
             "Rava humbly thanks them and spends his tidy profit on some nice juicy Veruzian apples " \
             "for Zephyr.")
+
+            print()
+
         elif(decision3 == "3"):
             print("WISE KNIGHT: The merchants find this idea too impractical " \
             "to adopt at first, but through further questioning, Rava " \
@@ -128,7 +147,9 @@ elif(decision1 == "Zephyr"):
             "craftsmanship, diplomacy, or fishing. The transition wasn't easy, " \
             "but Rava helped diversify the Alostrian economy, even if " \
             "just a little bit.")
+
             print()
+
         print("Rava returns to the castle after a hard day's work. " \
         "He is eager to meet Princess Rosalind. Rava wonders if she's going to " \
         "wear that perfume he secretly likes, but when he enters the royal court, " \
@@ -136,7 +157,7 @@ elif(decision1 == "Zephyr"):
         "An uneasy anxiety stirs in the halls until King Romir emerges from a " \
         "council meeting and announces that Princess Rosalind , sole heir " \
         "to the throne, has gone missing! Rava can't believe it... he won't settle " \
-        "for this. If it be his final breath, Rava will find the princess and" \
+        "for this. If it be his final breath, Rava will find the princess and " \
         "secure this kingdom's future...")
     
 
